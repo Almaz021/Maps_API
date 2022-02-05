95d6b6006beacee08f927532215cd793eaa11bec
elik <almaz.kzn2005@yandex.ru>
2022-02-05 16:53:50 +0300

    Part 2

--- main.py
+++ main.py
@@ -3,41 +3,59 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import requests
 from PIL import Image
 
 width, height = 1000, 850
-map_request = "http://static-maps.yandex.ru/1.x/?ll=49.099982%2C55.767306&spn=0.5,0.5&l=map"
-response = requests.get(map_request)
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.image = QLabel(self)
+        self.pixmap = None
+        self.spn = [0.5, 0.5]
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(100, 100, width, height)
+        self.setGeometry(300, 50, width, height)
         self.setWindowTitle('Map')
+        self.response()
+
+    def response(self):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll=49.099982%2C55.767306&spn={str(self.spn[0]) + ',' + str(self.spn[1])}&l=map"
+        response = requests.get(map_request)
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
         self.ran()
 
     def ran(self):
-        img = Image.open(map_file)
+        img = Image.open(self.map_file)
         print(img.size)
         img = img.resize((width, height))
-        img.save(map_file)
-        self.pixmap = QPixmap(map_file)
-        self.image = QLabel(self)
+        img.save(self.map_file)
+        self.pixmap = QPixmap(self.map_file)
         self.image.move(0, 0)
         self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and self.spn[0] > 0.0009765625:
+            self.spn[0] /= 2
+            self.spn[1] /= 2
+            print(self.spn)
+            self.response()
+        elif event.key() == Qt.Key_PageDown and self.spn[0] < 8:
+            self.spn[0] *= 2
+            self.spn[1] *= 2
+            print(self.spn)
+            self.response()
 
 
 if __name__ == '__main__':
@@ -45,3 +63,6 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
+# 16777238 Pg_up
+# 16777239 Pg_down
