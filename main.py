08c0db6f9440714bffcd4bb08273d2db223e0529
nadia <crocodile.who.wears.socks@yandex.ru>
2022-02-09 18:21:46 +0300

    part3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QPixmap
@@ -16,6 +16,7 @@ class Example(QWidget):
         self.image = QLabel(self)
         self.pixmap = None
         self.spn = [0.5, 0.5]
+        self.ll = [49.099982, 55.767306]
         self.initUI()
 
     def initUI(self):
@@ -24,21 +25,24 @@ class Example(QWidget):
         self.response()
 
     def response(self):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll=49.099982%2C55.767306&spn={str(self.spn[0]) + ',' + str(self.spn[1])}&l=map"
-        response = requests.get(map_request)
+        api_server = f"http://static-maps.yandex.ru/1.x/"
+        map_params = {'ll': f'{self.ll[0]},{self.ll[1]}',
+                      'spn': str(self.spn[0]) + ',' + str(self.spn[1]),
+                      'l': 'map'}
+        response = requests.get(api_server, params=map_params)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request + '?' + '&'.join([f'{k}={map_params[k]}' for k in map_params.keys()]))
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
         self.ran()
 
     def ran(self):
         img = Image.open(self.map_file)
-        print(img.size)
+        # print(img.size)
         img = img.resize((width, height))
         img.save(self.map_file)
         self.pixmap = QPixmap(self.map_file)
@@ -50,12 +54,23 @@ class Example(QWidget):
             self.spn[0] /= 2
             self.spn[1] /= 2
             print(self.spn)
-            self.response()
         elif event.key() == Qt.Key_PageDown and self.spn[0] < 8:
             self.spn[0] *= 2
             self.spn[1] *= 2
             print(self.spn)
-            self.response()
+        elif event.key() == Qt.Key_Up and self.ll[1] + self.spn[1] <= 180:
+            self.ll[1] += self.spn[1]
+        elif event.key() == Qt.Key_Down and self.ll[1] - self.spn[1] >= -180:
+            self.ll[1] -= self.spn[1]
+        elif event.key() == Qt.Key_Right and self.ll[0] + self.spn[0] <= 180:
+            self.ll[0] += self.spn[0]
+        elif event.key() == Qt.Key_Left and self.ll[0] - self.spn[0] >= -180:
+            self.ll[0] -= self.spn[0]
+        self.response()
+    
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
 
 
 if __name__ == '__main__':
